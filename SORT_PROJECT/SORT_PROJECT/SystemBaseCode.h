89d93497226bbf975c79ebc24efb54a3367f905d
Taylor3691 <hoangngoc3691@gmail.com>
2024-12-19 20:11:12 +0700

    finish 3 first command but havent test, quick sort have problem when n >100k

--- SORT_PROJECT/SORT_PROJECT/SystemBaseCode.h
+++ SORT_PROJECT/SORT_PROJECT/SystemBaseCode.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Header.h"
-
-pair<int,int> MeasureAlgorithm(int* arr, int n, string Algorithm_Name) {
-	int comparisons = 0;
+#include "utils.h"
+#include "Algorithms_Sort.h"
+pair<long long,long long> MeasureAlgorithm(int* arr, int n, string Algorithm_Name) {
+	long long comparisons = 0;
 	Algorithms algorithm;
 	auto start_time = std::chrono::steady_clock::now();
 	if (Algorithm_Name == "insertion-sort") {
@@ -39,42 +40,13 @@ pair<int,int> MeasureAlgorithm(int* arr, int n, string Algorithm_Name) {
 		algorithm.flashSort(arr, n, comparisons);
 	}
 	auto end_time = std::chrono::steady_clock::now();
-	auto duration = chrono::duration_cast<chrono::microseconds>(end_time - start_time).count();
+	auto duration = chrono::duration_cast<chrono::milliseconds>(end_time - start_time).count();
+	//cout << comparisons << "debug" << endl;
 	return make_pair(duration, comparisons);
 }
 
-void writeOutputArray(int* arr, int n, string filename) {
-	ofstream output(filename);
-	if (!output.is_open()) {
-		cout << "Can not open file\n";
-	}
 
-	output << n << "\n";
-	for (int i = 0; i < n; i++) {
-		output << arr[i] << " ";
-	}
-	output.close();
-}
-
-int* readInputArray(int& n, string File_Name) {
-	ifstream input(File_Name);
-	if (!input.is_open()) {
-		cout << "Can not open file\n";
-		return NULL;
-	}
-	input >> n;
-	int * array = new int[n];
-	for (int i = 0; i < n; i++)
-	{
-		input >> array[i];
-		//cout << array[i] << " ";
-	}
-	cout << endl;
-	input.close();
-	return array;
-}
-
-void printOutputVer_1(pair<int, int> runtime_comparison, string Output_Require) {
+void printOutputVer_1(pair <long long,long long> runtime_comparison, string Output_Require) {
 	if (Output_Require == "-both") 
 	{
 		cout << "Running time: " << runtime_comparison.first << endl;
@@ -95,11 +67,12 @@ void Command_1(int arvc, char* arvg[]) {
 	string File_Name = arvg[3];
 	string Output_Require = arvg[4];
 
-	// open file to read array
+	// Open file to read array
 	int n = 0;
 	int* array = NULL;
 	array = readInputArray(n, File_Name);
 	// Print input on CMD
+	cout << "Algorithm: " << Algorithm_Name << endl;
 	cout << "Input file: " << File_Name << endl;
 	cout << "Input size: " << n << endl;
 	cout << "------------------------------------\n";
@@ -117,35 +90,31 @@ void Command_1(int arvc, char* arvg[]) {
 	delete[] array;
 }
 
-int* generateArrayRequired(int Input_Size, string Input_Order) {
+int* generateArrayRequired(int Input_Size, string Input_Order, int* currentArray = NULL) {
 	srand(time(NULL));
 	Algorithms algorithm;
 	int comparison = 0;
+	// Allocate memory for array 
 	int* arr = new int[Input_Size];
-	for (int i = 0; i < Input_Size; i++) {
-		arr[i] = rand() % MAX_VALUE;
-	}
+	// Generate random array
+	GenerateRandomData(arr, Input_Size);
+	// if it require a random array
 	if (Input_Order == "-rand") {
 		return arr;
 	}
+	// if it require a sorted array, then sort array and return 
 	else if (Input_Order == "-sorted") {
-		algorithm.heapSort(arr, Input_Size, comparison);
+		GenerateSortedData(arr, Input_Size);
 		return arr;
 	}
+	// if it require a nearly sorted array, then sort array and swap some of couple of index in array
 	else if (Input_Order == "-nsorted") {
-		algorithm.heapSort(arr, Input_Size, comparison);
-		for (int i = 0; i < 10; i++) {
-			int r1 = rand() % Input_Size;
-			int r2 = rand() % Input_Size;
-			swap(arr[r1], arr[r2]);
-		}
+		GenerateNearlySortedData(arr, Input_Size);
 		return arr;
 	}
+	// if it require a descending array, then sort array and reverse it 
 	else if (Input_Order == "-rev") {
-		algorithm.heapSort(arr, Input_Size, comparison);
-		for (int i = 0; i <= Input_Size / 2; i++) {
-			swap(arr[i], arr[Input_Size - i - 1]);
-		}
+		GenerateReverseData(arr, Input_Size);
 		return arr;
 	}
 }
@@ -158,6 +127,7 @@ void Command_2(int arvc, char* arvg[]) {
 	string Output_Require = arvg[5];
 
 	// Print input on CMD
+	cout << "Algorithm: " << Algorithm_Name << endl;
 	cout << "Input size: " << Input_Size << endl;
 	cout << "Input order: " << Input_Order << endl;
 	cout << "------------------------------------\n";
@@ -183,7 +153,55 @@ void Command_2(int arvc, char* arvg[]) {
 }
 
 void Command_3(int arvc, char* arvg[]) {
+	// convert to string arguments
+	string Algorithm_Name = arvg[2];
+	int Input_Size = stoi((string)(arvg[3]));
+	string Output_Require = arvg[4];
 
+	// Print input on CMD
+	cout << "Algorithm: " << Algorithm_Name << endl;
+	cout << "Input size: " << Input_Size << endl<<endl;
+
+	// Creat 4 arrays in order as Random array, Nearly Sorted array, Sorted array and Reversed array
+	int* array_1 = generateArrayRequired(Input_Size, "-rand");
+	int* array_2 = creatNewArrayFromGivenArrayAsRequired(array_1, Input_Size, "-nsort");
+	int* array_3 = creatNewArrayFromGivenArrayAsRequired(array_1, Input_Size, "-sort");
+	int* array_4 = creatNewArrayFromGivenArrayAsRequired(array_1, Input_Size, "-rev");
+
+	// Write 4 kind of arrays into input_1.txt,...,input_4.txt
+	writeOutputArray(array_1, Input_Size, "input_1.txt");
+	writeOutputArray(array_2, Input_Size, "input_2.txt");
+	writeOutputArray(array_3, Input_Size, "input_3.txt");
+	writeOutputArray(array_4, Input_Size, "input_4.txt");
+
+	// Measure running time and comparison with created array
+	pair<long long, long long> runtime_comparison_1 = MeasureAlgorithm(array_1, Input_Size, Algorithm_Name);
+	pair<long long, long long> runtime_comparison_2	= MeasureAlgorithm(array_2, Input_Size, Algorithm_Name);
+	pair<long long, long long> runtime_comparison_3 = MeasureAlgorithm(array_3, Input_Size, Algorithm_Name);
+	pair<long long, long long> runtime_comparison_4 = MeasureAlgorithm(array_4, Input_Size, Algorithm_Name);
+
+	// Print result on CMD
+	cout << "Input order: Randomize\n";
+	cout << "------------------------------------\n";
+	printOutputVer_1(runtime_comparison_1, Output_Require);
+	cout << "\n";
+	cout << "Input order: Nearly Sorted\n";
+	cout << "------------------------------------\n";
+	printOutputVer_1(runtime_comparison_2, Output_Require);
+	cout << "\n";
+	cout << "Input order: Sorted\n";
+	cout << "------------------------------------\n";
+	printOutputVer_1(runtime_comparison_3, Output_Require);
+	cout << "\n";
+	cout << "Input order: Reversed\n";
+	cout << "------------------------------------\n";
+	printOutputVer_1(runtime_comparison_4, Output_Require);
+
+	// Release memory
+	delete[] array_1;
+	delete[] array_2;
+	delete[] array_3;
+	delete[] array_4;
 }
 
 void AlgorithmsMode(int arvc ,char* arvg[]) {
---
+++ SORT_PROJECT/SORT_PROJECT/utils.h
@@ -0,0 +1,92 @@
+#pragma once
+#include "Header.h"
+#include "Algorithms_Sort.h"
+void writeOutputArray(int* arr, int n, string filename) {
+	ofstream output(filename);
+	if (!output.is_open()) {
+		cout << "Can not open file\n";
+	}
+
+	output << n << "\n";
+	for (int i = 0; i < n; i++) {
+		output << arr[i] << " ";
+	}
+	output.close();
+}
+
+int* readInputArray(int& n, string File_Name) {
+	ifstream input(File_Name);
+	if (!input.is_open()) {
+		cout << "Can not open file\n";
+		return NULL;
+	}
+	input >> n;
+	int* array = new int[n];
+	for (int i = 0; i < n; i++)
+	{
+		input >> array[i];
+		//cout << array[i] << " ";
+	}
+	cout << endl;
+	input.close();
+	return array;
+}
+
+void GenerateRandomData(int* arr, int n) {
+	for (int i = 0; i < n; i++) {
+		arr[i] = rand() % MAX_VALUE;
+	}
+}
+
+void GenerateSortedData(int* arr, int n) {
+	long long comparison = 0;
+	Algorithms algorithm;
+	algorithm.heapSort(arr, n, comparison);
+}
+
+void GenerateReverseData(int* arr, int n) {
+	long long comparison = 0;
+	Algorithms algorithm;
+	algorithm.heapSort(arr, n, comparison);
+	for (int i = 0; i <= n / 2; i++) {
+		swap(arr[i], arr[n - i - 1]);
+	}
+}
+
+void GenerateNearlySortedData(int* arr, int n) {
+	long long comparison = 0;
+	Algorithms algorithm;
+	algorithm.heapSort(arr, n, comparison);
+	for (int i = 0; i < 10; i++) {
+		int r1 = rand() % n;
+		int r2 = rand() % n;
+		swap(arr[r1], arr[r2]);
+	}
+}
+
+int* creatNewArrayFromGivenArrayAsRequired(int* arr, int n, string demand) {
+	int* p = new int[n];
+	for (int i = 0; i < n; i++) {
+		p[i] = arr[i];
+	}
+	if (demand == "-rand") {
+		for (int i = 0; i < n; i++) {
+			int r1 = rand() % n;
+			int r2 = rand() % n;
+			swap(p[r1], p[r2]);
+		}
+		return p;
+	}
+	else if (demand == "-sort") {
+		GenerateSortedData(p, n);
+		return p;
+	}
+	else if (demand == "-nsort") {
+		GenerateNearlySortedData(p, n);
+		return p;
+	}
+	else if (demand == "-rev") {
+		GenerateReverseData(p, n);
+		return p;
+	}
+}
